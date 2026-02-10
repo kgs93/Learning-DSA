ec5183975218a08ae7ebb6dc3da9a54920913d1e
kgs93 <kgsekaran1404@gmail.com>
2026-02-10 14:53:42 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main{
     public static void main(String[] args){
-        int n=7679;
-        int count=0;
-        while(n>0){
-            n=n/10;
-            count+=1;
-
-        }
-        System.out.println(count);
+        System.out.println("Learning DSA");
     }
 }
