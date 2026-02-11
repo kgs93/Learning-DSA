d7b7640bec42cf5b17b0454165f66b3c951f7df3
kgs93 <kgsekaran1404@gmail.com>
2026-02-11 18:28:46 +0530

    Celing of a number problem

---
+++ src/BinarySearch/Celing_of_a_number.java
@@ -0,0 +1,32 @@
+package BinarySearch;
+
+public class Celing_of_a_number {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+        int target = 15;
+        int n = findNumber(arr, target);
+        System.out.println(n);
+    }
+    static int findNumber(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        if (target > arr[arr.length - 1]) {
+            return -1;
+        }
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return arr[mid];
+            }
+            else if (arr[mid] < target) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+
+        return arr[start];
+    }
+}
